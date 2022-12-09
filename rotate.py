05d092854e3787c1c0676277e224d9a28e72208c
SimeonOA <soa2j@mtmail.mtsu.edu>
2022-12-08 20:13:00 -0800

    add rotation

--- rotate.py
+++ rotate.py
@@ -19,5 +19,21 @@ SPEED=(.025,0.3*np.pi)
 iface=Interface("1703005",METAL_GRIPPER.as_frames(YK.l_tcp_frame,YK.l_tip_frame),
     ABB_WHITE.as_frames(YK.r_tcp_frame,YK.r_tip_frame),speed=SPEED)
 
-iface.go_cartesian(l_targets=[0, 0, 0.1])
-
+def rotate(angle, DEBUG=False):
+    # Rotates the left gripper by angle radians
+    for i in range(10):
+        if DEBUG:
+            print("loop", i)
+        l_cur = iface.y.left.get_pose()
+        l_delta = RigidTransform(
+            translation=np.array([0.0,0.0,0.0]),
+            # rotation=Interface.GRIP_DOWN_R,
+            rotation = np.array([[np.cos(angle),-np.sin(angle),0],[np.sin(angle),np.cos(angle),0],[0,0,1]]),
+            from_frame=YK.l_tcp_frame,
+            to_frame=YK.l_tcp_frame
+        )
+        l_new = l_cur*l_delta
+        iface.y.left.goto_pose(l_new,speed=iface.speed)
+    
+rotate(np.pi/6)
+print("done")
